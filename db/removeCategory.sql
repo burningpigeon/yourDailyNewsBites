8fbe72853920a8e392b80c76320431b716b4cd16
Pidge <pidgepurr@gmail.com>
2025-05-12 19:23:45 -0300

    Update removeCategory.sql

--- db/removeCategory.sql
+++ db/removeCategory.sql
@@ -1,22 +1,23 @@
-DELIMITER //
-DROP PROCEDURE IF EXISTS removeCategory //
-CREATE PROCEDURE removeCategory(
+CREATE OR REPLACE PROCEDURE remove_category(
     IN user_id_in INT,
     IN category_id_in INT
 )
+LANGUAGE plpgsql
+AS $$
 BEGIN
     IF NOT EXISTS (
         SELECT 1 FROM user_categories WHERE user_id = user_id_in AND category_id = category_id_in
     ) THEN
-        SIGNAL SQLSTATE '45000' 
-        SET MESSAGE_TEXT = 'User is not subscribed to this category';
+        RAISE EXCEPTION 'User is not subscribed to this category';
 
-    ELSEIF (SELECT COUNT(*) FROM user_categories WHERE user_id = user_id_in) = 1 THEN
-        SIGNAL SQLSTATE '45000' 
-        SET MESSAGE_TEXT = 'User must be subscribed to at least one category';
+    ELSIF (
+        SELECT COUNT(*) FROM user_categories WHERE user_id = user_id_in
+    ) = 1 THEN
+        RAISE EXCEPTION 'User must be subscribed to at least one category';
 
     ELSE
-        DELETE FROM user_categories WHERE user_id = user_id_in AND category_id = category_id_in;
+        DELETE FROM user_categories
+        WHERE user_id = user_id_in AND category_id = category_id_in;
     END IF;
-END //
-DELIMITER ;
+END;
+$$;
