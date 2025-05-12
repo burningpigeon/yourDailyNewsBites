33f44cf42a783a9a00963f0f558be2ce8436c561
Pidge <pidgepurr@gmail.com>
2025-05-12 19:21:31 -0300

    Update addCategory.sql

--- db/addCategory.sql
+++ db/addCategory.sql
@@ -1,28 +1,30 @@
-DELIMITER //
-DROP PROCEDURE IF EXISTS addCategory //
-CREATE PROCEDURE addCategory(
+CREATE OR REPLACE PROCEDURE add_category (
     IN user_id_in INT,
     IN category_id_in INT
 )
+LANGUAGE plpgsql
+AS $$
 BEGIN
     IF NOT EXISTS (
         SELECT 1 FROM users WHERE user_id = user_id_in
     ) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User not found';
+        RAISE EXCEPTION 'User not found';
     END IF;
 
     IF NOT EXISTS (
         SELECT 1 FROM categories WHERE category_id = category_id_in
     ) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Category not found';
+        RAISE EXCEPTION 'Category not found';
     END IF;
 
     IF EXISTS (
         SELECT 1 FROM user_categories WHERE user_id = user_id_in AND category_id = category_id_in
     ) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'User is already subscribed to this category';
+        RAISE EXCEPTION 'User is already subscribed to this category';
     ELSE
-        INSERT INTO user_categories (user_id, category_id)VALUES (user_id_in, category_id_in);
+        INSERT INTO user_categories (user_id, category_id)
+        VALUES (user_id_in, category_id_in);
     END IF;
-END //
-DELIMITER ;
+END;
+$$;
+
