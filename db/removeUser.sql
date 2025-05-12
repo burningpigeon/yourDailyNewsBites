1bcfc969fa415792de1557113dfdae34ab857ad3
Pidge <pidgepurr@gmail.com>
2025-05-12 18:53:44 -0300

    Update removeUser.sql

--- db/removeUser.sql
+++ db/removeUser.sql
@@ -5,11 +5,12 @@ LANGUAGE plpgsql
 AS $$
 BEGIN
     IF NOT EXISTS (
-        SELECT 1 FROM users WHERE email = emailIn 
+        SELECT 1 FROM users WHERE email = emailIn
     ) THEN
         RAISE EXCEPTION 'User not found';
     ELSE
-        DELETE FROM users WHERE email = emailIn 
+        DELETE FROM users WHERE email = emailIn;
     END IF;
 END;
 $$;
+
