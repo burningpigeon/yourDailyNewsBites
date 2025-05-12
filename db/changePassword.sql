91220d01968b2ee444ef38e1d9e43db0150068f1
Pidge <pidgepurr@gmail.com>
2025-05-12 19:04:12 -0300

    Update changePassword.sql

--- db/changePassword.sql
+++ db/changePassword.sql
@@ -1,16 +1,19 @@
-DELIMITER //
-DROP PROCEDURE IF EXISTS changePassword //
-CREATE PROCEDURE changePassword(
+CREATE OR REPLACE PROCEDURE change_password (
     IN user_id_in INT,
     IN currentPasswordHash VARCHAR(255), 
     IN newPasswordHash VARCHAR(255)
 )
+LANGUAGE plpgsql
+AS $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM users WHERE user_id = user_id_in AND password_hash = currentPasswordHash) THEN
-        SIGNAL SQLSTATE '45000' 
-        SET MESSAGE_TEXT = 'Incorrect current password';
+    IF NOT EXISTS (
+        SELECT 1 FROM users WHERE user_id = user_id_in AND password_hash = currentPasswordHash
+    ) THEN
+        RAISE EXCEPTION 'Incorrect current password';
     END IF;
 
-    UPDATE users SET password_hash = newPasswordHash WHERE user_id = user_id_in;
-END //
-DELIMITER ;
+    UPDATE users
+    SET password_hash = newPasswordHash
+    WHERE user_id = user_id_in;
+END;
+$$;
